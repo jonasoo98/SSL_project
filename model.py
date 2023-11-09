f632700d040913636a748e2e595e06876e9c3973
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2023-11-09 11:15:58 +0100

    add downstream

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torchvision import models
 
@@ -30,21 +31,68 @@ class ProjectionHead(nn.Module):
         return x
 
 
-class BackboneModel(nn.Module):
+class ContrastiveLearningModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = models.resnet50()
-
-        # Changing the layers like mentioned in the article
-        self.resnet.maxpool = nn.Sequential()
-        self.resnet.fc = nn.Sequential()
-        self.resnet.conv1 = nn.Conv2d(
-            3, 64, kernel_size=(3, 3), stride=(1, 1), bias=False
-        )
-
+        self.encoder = self.get_encoder()
         self.projection_head = ProjectionHead(2048, 2048, 128)
 
     def forward(self, x):
-        x = self.resnet(x)
+        x = self.encoder(x)
         x = self.projection_head(torch.squeeze(x))
+        return x
+
+    def get_encoder(self):
+        resnet = models.resnet50()
+        resnet.maxpool = nn.Identity()
+        resnet.fc = nn.Identity()
+        resnet.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), bias=False)
+        return resnet
+
+
+class DownstreamModel(nn.Module):
+    def __init__(
+        self,
+        encoder: nn.Module,
+        model_head: nn.Module,
+        num_classes: int,
+        batch_size: int,
+    ):
+        super().__init__()
+
+        self.encoder = encoder
+        self.model_head = model_head
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+        for param in self.model_head.parameters():
+            param.requires_grad = True
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        reshaped_encoded = encoded.view(self.batch_size, 2, 32, 32)
+        output = self.model_head(reshaped_encoded)
+        return output
+
+
+class BaselineModel(nn.Module):
+    def __init__(self, num_channels: int):
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
